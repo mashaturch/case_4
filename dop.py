199adce4a971f0e16cc844f570cecefb37a3b962
mashaturch <s1yguvzg>
2021-02-14 14:53:56 +0700

    counted the number of words

--- dop.py
+++ dop.py
@@ -1,3 +1,5 @@
 from main import text
-count_sentens = text.count('.')
+count_sentens = text.count('.') + text.count('!') + text.count('?')
 ASL = count_words/count_sentens
+count_words = text.count(" ") + 1
+ASW = count_syllables/count_words

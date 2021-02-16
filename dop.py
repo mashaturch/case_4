ed8d053076e1ed20f83eb9db97bd816dbc39a357
Tanuaaz <t.zubareva@g.nsu.ru>
2021-02-16 19:28:52 +0700

    changes

--- dop.py
+++ dop.py
@@ -7,10 +7,10 @@ ASW = count_syllables/count_words
 count_syllables = 0
 k = 0
 while k < len(text):
-    if text[k] in 'aeiouAEIOUауоиэыяюеёАУОИЭЫЯЮЕЁ':
+    if (text[k] in 'aeiouAEIOUауоиэыяюеёАУОИЭЫЯЮЕЁ'):
         count_syllables += 1
     k = k+1
-if text[0] in ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ':
+if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'):
     FRE = 206.835-(1.3*ASL)-(60.1*ASW)
 else:
     FRE = 206.835-1.015(count_words/count_sentens)-84.6(count_syllables/count_words)

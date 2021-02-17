34e0b9213b040c85e8e65e5e21ee3beb7fa110f6
Tanuaaz <t.zubareva@g.nsu.ru>
2021-02-17 17:55:50 +0700

    changes 2

--- dop.py
+++ dop.py
@@ -2,15 +2,15 @@ from main import text
 
 count_sentens = text.count('.') + text.count('!') + text.count('?')
 count_words = text.count(" ") + 1
-ASL = count_words/count_sentens
-ASW = count_syllables/count_words
+ASL = float(count_words/count_sentens)
+ASW = float(count_syllables/count_words)
 count_syllables = 0
 k = 0
 while k < len(text):
     if (text[k] in 'aeiouAEIOUауоиэыяюеёАУОИЭЫЯЮЕЁ'):
         count_syllables += 1
     k = k+1
-if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'):
+if (text[0] in ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'):
     FRE = 206.835-(1.3*ASL)-(60.1*ASW)
 else:
-    FRE = 206.835-1.015(count_words/count_sentens)-84.6(count_syllables/count_words)
+    FRE = 206.835-1.015*ASL-84.6*ASW

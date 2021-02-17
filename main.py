186c9f90a58ab7f71f1577e70c9f713c11e5854a
kozty <m.kostylev@g.nsu.ru>
2021-02-17 17:52:30 +0700

    1234

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import dop as d
 text = input("Введите текст:")
 
 
-print ('Предложений:', count_sentens)
-print ('Слов:', count_words)
-print ('Слогов:', count_syllables)
-print ('Средняя длина предложения в словах:', ASL)
-print ('Средняя длина слова в слогах:', ASW)
-print ('Индекс удобочитаемости Флеша:', FRE)
+print ('Предложений:', d.count_sentens)
+print ('Слов:', d.count_words)
+print ('Слогов:', d.count_syllables)
+print ('Средняя длина предложения в словах:', d.ASL)
+print ('Средняя длина слова в слогах:', d.ASW)
+print ('Индекс удобочитаемости Флеша:', d.FRE)
 

16f12c73ff1bfcf7ae0a7513fb005acdaba05126
mashaturch <s1yguvzg>
2021-02-20 11:32:17 +0700

    tonality and subjectivity

--- main.py
+++ main.py
@@ -13,14 +13,17 @@ ASL = float(count_words/count_sentens)
 count_syllables = 0
 k = 0
 while k < len(text):
-    if (text[k] in 'aeiouAEIOUауоиэыяюеёАУОИЭЫЯЮЕЁ'):
+    if (text[k] in 'aeiouyAEIOUYауоиэыяюеёАУОИЭЫЯЮЕЁ'):
         count_syllables += 1
     k = k+1
 ASW = float(count_syllables/count_words)
-if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'):
+if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'):  #russian text
     FRE = 206.835-(1.3*ASL)-(60.1*ASW)
+    text_trans = TextBlob(text)
+    text_trans = text_trans.translate()
 else:
-    FRE = 206.835-(1.015*ASL)-(84.6*ASW)
+    FRE = 206.835-(1.015*ASL)-(84.6*ASW)  #english text
+    text_trans = TextBlob(text)
 
 
 print ('Предложений:', count_sentens)
@@ -29,12 +32,13 @@ print ('Слогов:', count_syllables)
 print ('Средняя длина предложения в словах:', ASL)
 print ('Средняя длина слова в слогах:', ASW)
 print ('Индекс удобочитаемости Флеша:', FRE)
-blob = TextBlob(text)
-trans = text
-print (trans)
-print ('Тональность текста: ')
-if (blob.detect_language() != 'en'):
-    trans = blob.translate(to = 'en')
-    print ('Объективность:', (1 - trans.subjectivity))
+
+print (text_trans)
+if text_trans.polarity >= 0.3:
+    tonality = 'положительная'
+elif -0.3 <= text_trans.polarity <= 0.3:
+    tonality = 'нейтральная'
 else:
-    print ('Объективность:', (1 - TextBlob(text).subjectivity))
+    tonality = 'негативная'
+print ('Тональность текста: ', tonality)
+print ('Объективность: {:.1f}%'.format ((1 - text_trans.subjectivity) * 100))

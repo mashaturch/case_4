2e1b3c435aed4ae5865ddf202a31fa9e19616ba2
mashaturch <s1yguvzg>
2021-02-20 11:48:37 +0700

    comments

--- main.py
+++ main.py
@@ -5,18 +5,22 @@
 """
 
 from textblob import TextBlob
+
+#text input
 text = input("Введите текст:")
 
-count_sentens = text.count('.') + text.count('!') + text.count('?')
-count_words = text.count(" ") + 1
+count_sentens = text.count('.') + text.count('!') + text.count('?')  #number of proposals
+count_words = text.count(" ") + 1  #word count
 ASL = float(count_words/count_sentens)
 count_syllables = 0
 k = 0
+#number of syllables
 while k < len(text):
     if (text[k] in 'aeiouyAEIOUYауоиэыяюеёАУОИЭЫЯЮЕЁ'):
         count_syllables += 1
     k = k+1
 ASW = float(count_syllables/count_words)
+
 if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'):  #russian text
     FRE = 206.835-(1.3*ASL)-(60.1*ASW)
     text_trans = TextBlob(text)
@@ -25,7 +29,7 @@ else:
     FRE = 206.835-(1.015*ASL)-(84.6*ASW)  #english text
     text_trans = TextBlob(text)
 
-
+#output of results
 print ('Предложений:', count_sentens)
 print ('Слов:', count_words)
 print ('Слогов:', count_syllables)
@@ -33,7 +37,7 @@ print ('Средняя длина предложения в словах:', ASL)
 print ('Средняя длина слова в слогах:', ASW)
 print ('Индекс удобочитаемости Флеша:', FRE)
 
-if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'):
+if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'): #reading for Russian text
     if FRE > 80:
         print ('Текст очень легко читается (для младших школьников).')
     elif FRE > 50:
@@ -43,11 +47,14 @@ if (text[0] in 'ЙЦУКЕЁНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБ
     else:
         print ('Текст трудно читается (для выпускников ВУЗов).')
 
+#finding the tonality of the text
 if text_trans.polarity >= 0.3:
     tonality = 'положительная'
 elif -0.3 <= text_trans.polarity <= 0.3:
     tonality = 'нейтральная'
 else:
     tonality = 'негативная'
-print ('Тональность текста: ', tonality)
+
+#output of results
+print ('Тональность текста:', tonality)
 print ('Объективность: {:.1f}%'.format ((1 - text_trans.subjectivity) * 100))
